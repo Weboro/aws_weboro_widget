0756b7c19c3a6a85c986365e6358c25fce198c76
Scelester <nabinpauudel664@gmail.com>
2024-03-09 14:16:09 +0545

    added_headless

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')  # Enable headless mode
+chrome_options.add_argument('--no-sandbox')  # Disable sandbox mode (not recommended for security reasons)
+chrome_options.add_argument('--disable-dev-shm-usage')
+
 from time import sleep
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://clickconsultingnepal.com/scrape/googlescraper?place_id=ChIJUW-zCOkZ6zkRnadg7FRw22s")
 
 # divs = driver.find_elements_by_tag_name('div')
