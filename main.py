0df1c7c56a7210181ea5c4b5ea5656e05eb37c53
Scelester <nabinpauudel664@gmail.com>
2024-03-14 21:21:59 +0545

    gg

--- main.py
+++ main.py
@@ -26,49 +26,50 @@ headers = {
 
 count = 0
 log = "________________________________For Google_____________________________"
+print("--------------------------------For Google________________________")
 
-
-# for user in fetch_data_from_api(f"{domain}/api/userswith/google_reviews_api",api_key=api_key):
-#     count += 1
+for user in fetch_data_from_api(f"{domain}/api/userswith/google_reviews_api",api_key=api_key):
+    count += 1
     
-#     print(f"For :{count}. {user['name']}")
-#     log += f"For :{count}. {user['name']}\n"
+    print(f"For :{count}. {user['name']}")
+    log += f"For :{count}. {user['name']}\n"
 
-#     user_api_key = user['api_token']
-#     data = json.loads(user['service_kvpairs'])
-#     places_id = data['google reviews api']['places_id']
+    user_api_key = user['api_token']
+    data = json.loads(user['service_kvpairs'])
+    places_id = data['google reviews api']['places_id']
     
-#     data = googlewidget.get_data(api_key,user_api_key,places_id)
+    data = googlewidget.get_data(api_key,user_api_key,places_id)
 
-#     if data == []:
-#         print("Error: check for : Invalid placeid")
-#         log += f"Error: check for - Invalid placeid\n\n"
-#     else:
+    if data == []:
+        print("Error: check for : Invalid placeid")
+        log += f"Error: check for - Invalid placeid\n\n"
+    else:
 
-#         fulldata = {
-#             'user_key': user_api_key,
-#             'aws_key':api_key,
-#             'api_data': data
-#         }
+        fulldata = {
+            'user_key': user_api_key,
+            'aws_key':api_key,
+            'api_data': data
+        }
 
-#         # print(f"{domain}/api/store/google_reviews_api/")
+        # print(f"{domain}/api/store/google_reviews_api/")
 
         
 
-#         response =  requests.post(f"{domain}/api/ssstore/google_reviews_api/",json=fulldata,headers=headers)
+        response =  requests.post(f"{domain}/api/ssstore/google_reviews_api/",json=fulldata,headers=headers)
 
-#         res = response.json()
-#         print(res)
+        res = response.json()
+        print(res)
 
-#         log += str(res) + "\n\n"
+        log += str(res) + "\n\n"
 
-#     print("\n")
+    print("\n")
 
 
-# googlewidget.close()
+googlewidget.close()
 
 
 log = "________________________________For Tiktok_____________________________"
+print("--------------------------------For Tiktok________________________")
 count = 0
 for user in fetch_data_from_api(f'{domain}/api/userswith/tiktok_api',api_key=api_key):
     count += 1
@@ -86,7 +87,15 @@ for user in fetch_data_from_api(f'{domain}/api/userswith/tiktok_api',api_key=api
 
     response =  requests.post(f"{domain}/api/ssstore/tiktok_api/",json=data,headers=headers)
     
-    print(response)
+    if response.status_code != 200:
+        log += f"Something went wrong"
+        print("Something went wrong")
+    else:
+        res = response.json()
+        print(response.json())
+        log += str(res) + "\n\n"
+    
+    print("\n")
 
 
 
